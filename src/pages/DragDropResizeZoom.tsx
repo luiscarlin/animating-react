9ac2c786d27c61c155092f99f41fc2faaaab024c
Luis Carlin <luiscarlin@gmail.com>
2022-04-18 10:56:56 -0400

    place element as relative

--- src/pages/DragDropResizeZoom.tsx
+++ src/pages/DragDropResizeZoom.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import styled from "styled-components";
+
 const DragDropResizeZoom = () => {
-  return <>hey</>;
+  const [pos, setPos] = useState({ x: 50, y: 50 });
+  return (
+    <Page>
+      <Element style={{ position: "relative", top: pos.x, left: pos.y }}>
+        hello
+      </Element>
+    </Page>
+  );
 };
+
+const Page = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
+
+const Element = styled.div`
+  height: 100px;
+  width: 100px;
+  background: red;
+`;
 
 export default DragDropResizeZoom;
